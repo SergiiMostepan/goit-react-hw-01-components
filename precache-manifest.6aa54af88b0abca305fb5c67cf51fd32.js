7798f55f29a5959377d0b30559c96acbf64bf6f7
Sergii Mostepan <sergiimostepan@gmail.com>
2020-03-11 22:24:10 +0200

    Updates

---
+++ precache-manifest.6aa54af88b0abca305fb5c67cf51fd32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f833c5ecfeea65ae162c39e5bb1959",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ea0c918e7978bbc8212a",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "15042eef5e04fc7bee00",
+    "url": "/goit-react-hw-01-components/static/css/main.c01e35b9.chunk.css"
+  },
+  {
+    "revision": "ea0c918e7978bbc8212a",
+    "url": "/goit-react-hw-01-components/static/js/2.94ebe6f9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-01-components/static/js/2.94ebe6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15042eef5e04fc7bee00",
+    "url": "/goit-react-hw-01-components/static/js/main.453c448c.chunk.js"
+  },
+  {
+    "revision": "a62329f2cc0b24bc37f9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.39b5137b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6239128ef54cb0b927a5c60cb741db4f.js"
+  "/goit-react-hw-01-components/precache-manifest.6aa54af88b0abca305fb5c67cf51fd32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
